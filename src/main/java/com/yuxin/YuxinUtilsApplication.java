5facda2e7c18e0f91db732f88d2728daa4dc4f23
yuxin <yuxinw666@gmail.com>
2020-12-12 12:04:29 +0800

    init yuxin-utils

---
+++ src/main/java/com/yuxin/YuxinUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.yuxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuxinUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuxinUtilsApplication.class, args);
+    }
+
+}
